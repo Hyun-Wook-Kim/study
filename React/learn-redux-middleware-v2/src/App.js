2245d8cf4c5442f08fb80da537ea7346488f9baf
Hyun-Wook-Kim <83201830+Hyun-Wook-Kim@users.noreply.github.com>
2022-05-10 09:27:55 +0900

    react

---
+++ React/learn-redux-middleware-v2/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import CounterContainer from './container/CounterContainer';
+import SampleContainer from './container/SampleContainer';
+
+function App() {
+  return (
+    <SampleContainer></SampleContainer>
+    );
+}
+
+export default App;
---
+++ React/learn-redux-middleware-v2/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk'
+
+const logger = createLogger();
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+
+root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
